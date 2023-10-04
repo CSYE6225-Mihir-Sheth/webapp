b62e0c7e725de9fad6acf440b1ed47aa232ac7fc
Mihir <sheth.mi@northeastern.edu>
2023-10-04 16:57:27 -0400

    test and changed server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from './applicaiton/application.js';  
 import config from './applicaiton/database/dbConfig.js';
 
-const port = 8080;  //initialized a port to be used
+const port = process.env.port;  //initialized a port to be used
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);   //main application.js file listens to the server.
