f0066286dc275ccd8c7c95c68f5871c22a023015
Mihir <sheth.mi@northeastern.edu>
2023-10-04 18:42:56 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import app from './applicaiton/application.js'; 
+import config from './applicaiton/database/dbConfig.js'
 
-const port = process.env.port;  //initialized a port to be used
+const port = config.dbC.port;
+// const port = process.env.port;  //initialized a port to be used
 
 const server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);   //main application.js file listens to the server.
