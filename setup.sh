2431401710b9d0b8e2aea8d96c3e1dfdcbbea096
MihirSheth77 <114521572+MihirSheth77@users.noreply.github.com>
2023-10-17 02:56:32 -0400

    Demo (#9)
    * all functionalities
    
    * test
    
    * test
    
    * test
    
    * Test
    
    * Test
    
    * test and changed server
    
    * Testa and changes
    
    * test
    
    * modified some
    
    * YML UPDATE
    
    * YML
    
    * yml
    
    * secret
    
    * port
    
    * workflow
    
    * updated server.js
    
    * workflow
    
    * workflow
    
    * yml change
    
    * test
    
    * port
    
    * updates
    
    * updated
    
    * updates
    
    * yml
    
    * mysql v
    
    * host
    
    * run test delay
    
    * test
    
    * test1
    
    * test2
    
    * testing
    
    * readme
    
    * Readme
    
    * new changes
    
    * new changes
    
    * changes made
    
    * changes
    
    * deleted files
    
    * workflows and secrets
    
    * updated changes
    
    * workflow updates
    
    * workflow changed
    
    * changes made
    
    * workflow
    
    * workflow trim
    
    * new
    
    * new
    
    * plugins
    
    * workflow
    
    * webapp zip location
    
    * last

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install unzip
+sudo apt-get install nodejs npm -y
+sudo apt-get install mariadb-server -y
+sudo mysql -u root -e "SET PASSWORD 'root'@'localhost' = PASSWORD(root123');"
+printf 'root123\nn\nn\nn\nn\nn\ny\n' | sudo mysql_secure_installation;
+sudo mysql -e "GRANT ALL PRIVILEGES ON healthCheck.* TO 'root'@'localhost' IDENTIFIED BY 'root123';"
+sudo mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -proot123 -Bse "CREATE DATABASE healthCheck;"
+mysql -u root -proot123 -Bse "SHOW DATABASES;"
+sudo mkdir opt
+sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
+sudo mv /home/admin/users.csv /home/admin/opt/users.csv
+cd opt
+sudo unzip -o webapp.zip
+cd webapp
+sudo npm install
