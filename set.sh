abb850bc21df4e746fe91d4cf1ff2b283d9b6bf6
Mihir <sheth.mi@northeastern.edu>
2023-11-08 00:24:21 -0500

    changes

--- set.sh
+++ set.sh
@@ -9,14 +9,12 @@ sudo wget https://amazoncloudwatch-agent.s3.amazonaws.com/debian/amd64/latest/am
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
-sudo cp /home/admin/webapp.service /etc/systemd/system/
+sudo cp /home/admin/app.service /etc/systemd/system/
 sudo mv /home/admin/webapp.zip /opt/csye6225/webapp.zip
 sudo unzip -o /opt/csye6225/webapp.zip -d /opt/csye6225/webapp
 sudo cp /home/admin/cloudwatch-config.json /opt/csye6225/webapp/cloudwatch-config.json
 sudo cp /opt/csye6225/webapp/cloudwatch-config.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
-ls
 cd /opt/csye6225/webapp
-ls
 sudo mv /home/admin/webapp.zip /opt/csye6225/webapp.zip
 sudo npm i
 sudo systemctl daemon-reload
