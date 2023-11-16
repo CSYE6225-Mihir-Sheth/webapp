ac7c3a7ed69cac746a6f6f6831ba5e88f5f5a1ac
Mihir <sheth.mi@northeastern.edu>
2023-11-15 20:21:42 -0500

    changes

--- set.sh
+++ set.sh
@@ -4,6 +4,8 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install nodejs npm -y
+sudo wget https://amazoncloudwatch-agent.s3.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
@@ -20,8 +22,6 @@ cd /opt/csye6225
 sudo npm i
 sudo cp /home/admin/app.service /etc/systemd/system/
 
-sudo wget https://amazoncloudwatch-agent.s3.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb
-sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 sudo systemctl daemon-reload
 sudo systemctl enable app.service
 sudo systemctl start app.service
